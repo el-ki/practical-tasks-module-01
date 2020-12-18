a0916f445f23a3437948b98f00a6d286fbfacdb9
Elena Kichinskaia <nitress@gmail.com>
2020-12-18 14:33:27 +0200

    20 tasks completed

---
+++ js/task-11.js
@@ -0,0 +1,16 @@
+// Задание
+// Функция calculateTotalPrice считает и возвращает общую сумму покупки.Она принимает два параметра,
+//     значения которых будут задаваться во время её вызова.
+
+// orderedQuantity - количество единиц товара в заказе;
+// pricePerItem - цена одной единицы товара.
+// Дополни код функции так, чтобы в переменную totalPrice записывалась общая сумма покупки,
+//     результат умножения кол - ва товаров на цену одного.
+
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  const totalPrice = orderedQuantity * pricePerItem;
+
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice(4, 25));
---
+++ js/task-19.js
@@ -0,0 +1,31 @@
+// Задание
+// Функция checkPassword(password) получает пароль пользователя в параметр password,
+//     проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и
+// возвращает сообщение о результате сравнения, хранящееся в переменной message.
+
+// Если значение параметра password равно null, значит пользователь отменил операцию и в message записывается строка
+// 'Отменено пользователем!'.
+// Если значение параметра password совпадает со значением ADMIN_PASSWORD, в переменную message присваивается строка
+// 'Добро пожаловать!'.
+// Если ни одно из предыдущих условий не выполнилось, в переменную message записывается строка
+// 'Доступ запрещен, неверный пароль!'.
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) { // Дополни эту строку
+    message =  'Отменено пользователем!';
+  } else if (password === ADMIN_PASSWORD) { // Дополни эту строку
+    message = 'Добро пожаловать!';
+  } else {
+    message = 'Доступ запрещен, неверный пароль!';
+  }
+
+  return message;
+}
+
+console.log(checkPassword('mangohackzor')) // 'Доступ запрещен, неверный пароль!'.
+console.log(checkPassword(null)) // 'Отменено пользователем!'.
+console.log(checkPassword('polyhax')) // 'Доступ запрещен, неверный пароль!'.
+console.log(checkPassword('jqueryismyjam')) // 'Добро пожаловать!'.
